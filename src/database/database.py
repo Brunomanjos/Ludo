10f4c11425ddeb8871e106e276df1746519ea6b7
Brunomanjos <58401611+Brunomanjos@users.noreply.github.com>
2020-11-09 23:11:54 -0300

    Início Database

--- src/database/database.py
+++ src/database/database.py
@@ -1,1 +1,49 @@
-from mysql.connector import *
+from mysql.connector import connect, Error
+
+__all__ = ['execute', 'execute_and_close']
+
+connection = None
+
+
+def init():
+    global connection
+    try:
+        connection = connect(host='localhost', user='root', password='root')
+        cursor = connection.cursor()
+        cursor.execute('create database if not exists Modular')
+        cursor.close()
+        connection.close()
+        connection = connect(host='localhost', database='Modular', user='root', password='root')
+        raise ArithmeticError('no')
+    except Error as e:
+        print(f'Erro ao se conectar ao banco de dados: {e.msg}')
+
+
+def execute(sql):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        connection.commit()
+        return cursor
+    except Error as e:
+        print(f'Erro ao executar o comando sql: {e.msg}')
+
+
+def execute_and_close(sql):
+    cursor = execute(sql)
+
+    if not cursor:
+        return False
+
+    cursor.close()
+    return True
+
+
+def close():
+    global connection
+    if connection:
+        connection.close()
+    connection = None
+
+
+init()
---
+++ src/tests/database_test.py
@@ -0,0 +1,17 @@
+# Teste Automatizado do módulo Database
+# Atualizado: 09/11/2020
+# Autor: Bruno Messeder dos Anjos
+
+import unittest
+
+import database
+
+
+class DatabaseTest(unittest.TestCase):
+
+    def test_01_clear_table(self):
+        self.assertTrue(database.execute_and_close('delete from TestTable'))
+
+
+if __name__ == '__main__':
+    unittest.main()
