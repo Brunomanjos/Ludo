6926584beff16c5ab17f3b33d1cafae51c51588b
Brunomanjos <58401611+Brunomanjos@users.noreply.github.com>
2020-12-08 18:09:09 -0300

    Update gif.py

--- src/gui/sprite/gif.py
+++ src/gui/sprite/gif.py
@@ -26,17 +26,18 @@ class GIF(pygame.sprite.Sprite):
         self._secs_on_screen = gif.info['duration'] / 1000
         self._last_img_time = time.time()
         self.loop = loop
-        self._running = None
+        self._running = run_at_start
+        self._loading = True
         self.on_end = on_end
 
         self.images = []
         self._image_index = -1
-        Thread(target=self._load_all_images, args=[gif, run_at_start]).start()
+        Thread(target=self._load_all_images, args=[gif]).start()
 
         self.image = pygame.Surface(gif.size)
         self.rect = self.image.get_rect(**pos)
 
-    def _load_all_images(self, gif, run_at_start):
+    def _load_all_images(self, gif):
         with load_lock:
             for frame in range(gif.n_frames):
                 gif.seek(frame)
@@ -50,20 +51,24 @@ class GIF(pygame.sprite.Sprite):
                 else:
                     self.images.append(pygame.image.load(image))
                     self.image = self.images[0]
-
-            self._running = run_at_start
+            self._loading = False
             gif.close()
 
     def update(self):
+        if not self._running:
+            return
+
         now = time.time()
 
-        if not self._running or now - self._last_img_time < self._secs_on_screen:
+        if now - self._last_img_time < self._secs_on_screen:
             return
 
         self._last_img_time = now
         self._image_index += 1
 
         if self._image_index >= len(self.images):
+            if self._loading:
+                return
             if self.on_end:
                 self.on_end()
             if not self.loop:
@@ -77,3 +82,6 @@ class GIF(pygame.sprite.Sprite):
         if self._running is False:
             self._running = True
             self._image_index = -1
+
+    def stop(self):
+        self._running = False
